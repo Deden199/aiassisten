868b387459c8ea73808102ca56c5b33dbaa68401
Nail <92734420+Deden199@users.noreply.github.com>
2025-08-12 23:43:17 +0700

    chore: drop axios in favor of fetch

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,1 @@
-import axios from 'axios';
-window.axios = axios;
-
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+// Axios has been removed; HTTP requests now use the Fetch API.
